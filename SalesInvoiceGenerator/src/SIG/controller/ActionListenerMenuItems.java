4f00d69b0cae8711b7c6abee9c4bd6fcc81c6ed5
MaryMalak <Maryloka142@gmail.com>
2022-04-17 19:47:57 +0200

    Add LoadFile method

--- SalesInvoiceGenerator/src/SIG/controller/ActionListenerMenuItems.java
+++ SalesInvoiceGenerator/src/SIG/controller/ActionListenerMenuItems.java
@@ -4,9 +4,22 @@
  * and open the template in the editor.
  */
 package SIG.controller;
+import SIG.model.InvoiceHeader;
 import SIG.view.SIG_Frame;
+//import java.awt.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import javax.swing.JFileChooser;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List; 
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,6 +27,7 @@ import java.awt.event.ActionListener;
  */
 public class ActionListenerMenuItems implements ActionListener{
     private SIG_Frame frame;
+    private SimpleDateFormat date = new SimpleDateFormat("dd-MM-yyyy");
 
     public ActionListenerMenuItems(SIG_Frame frame) {
         this.frame = frame;
@@ -24,12 +38,39 @@ public class ActionListenerMenuItems implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         switch (e.getActionCommand()){
             case "Load File":
-                System.out.println("Load File");
+                LoadFile();
                 break;
             case "Save File":
-                System.out.println("Save File");
+                SaveFile();
                 break;
         }
+    }
+    private void LoadFile() {
+        JFileChooser fileChooser=new JFileChooser();
+        try{
+        int choice= fileChooser.showOpenDialog(frame);
+        if (choice==JFileChooser.APPROVE_OPTION) {
+            Path filePath=Paths.get(fileChooser.getSelectedFile().getAbsolutePath());
+           List<String> lines;
+            lines = Files.readAllLines(filePath);
+            ArrayList<InvoiceHeader> Headers = new ArrayList<>();
+            for(String line :lines){
+                String[]elements=line.split(",");
+                String element1=elements[0]; 
+                int code =Integer.parseInt(elements[0]);
+                Date invoiceDate = date.parse(elements[1]);
+                InvoiceHeader header = new InvoiceHeader(code, invoiceDate, elements[2]);
+                    Headers.add(header);      
+            }
+        }
+        }catch(IOException e){
+            JOptionPane.showMessageDialog(frame, e.getMessage(),"Error", JOptionPane.ERROR_MESSAGE);
+        }catch(ParseException e){
+            JOptionPane.showMessageDialog(frame, e.getMessage(),"Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void SaveFile() {
     }
     
 }
